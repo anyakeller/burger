b9250ff534242fe7441387365815a37300ab27df
Anya L Keller <kelleranyal@gmail.com>
2019-10-26 12:58:58 -0400

    Asdfdsa

--- config/connection.js
+++ config/connection.js
@@ -3,9 +3,9 @@ var mysql = require('mysql');
 var connection;
 
 if (process.env.JAWSDB_URL) {
-  mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  mysql.createConnection({
+  connection  = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
