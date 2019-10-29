f9ecc3e603deb031a69d23fed3b9ccf324dffa27
Anya L Keller <kelleranyal@gmail.com>
2019-10-29 18:57:53 -0400

    test

--- config/connection.js
+++ config/connection.js
@@ -1,30 +1,27 @@
-var mysql = require('mysql');
+var mysql = require("mysql");
 
 var connection;
 
 console.log(process.env.JAWSDB_URL);
 
 if (process.env.JAWSDB_URL) {
-	connection = mysql.createConnection({
-		host: process.env.JAWSDB_URL,
-		dialect: 'mysql'
-	});
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'password',
-    database: 'burgers_db'
-  });
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "password",
+        database: "burgers_db"
+    });
 }
 
 connection.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  }
-  console.log('connected as id ' + connection.threadId);
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
 });
 
 module.exports = connection;
